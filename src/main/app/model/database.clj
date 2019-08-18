ee6d37da0f422e4eb4935cbf3b2c79bde479fa30
Piotr Roterski <pt.roterski@gmail.com>
2019-08-18 17:50:24 +0200

    Replace memdb with embedded kafka

--- src/main/app/model/database.clj
+++ src/main/app/model/database.clj
@@ -1,15 +1,32 @@
 (ns app.model.database
-  (:require [crux.api :as crux])
+  (:require [crux.api :as crux]
+            [crux.kafka.embedded :as ek]
+            [mount.core :refer [defstate]])
   (:import (crux.api ICruxAPI)))
 
-(def ^crux.api.ICruxAPI node
-  (crux/start-standalone-node {:kv-backend "crux.kv.memdb.MemKv"
-                               :db-dir "data/db-dir-1"
-                               :event-log-dir "data/eventlog-1"}))
+; (def ^crux.api.ICruxAPI node
+;  (crux/start-standalone-node {:kv-backend "crux.kv.memdb.MemKv"
+;                               :db-dir "data/db-dir-1"
+;                               :event-log-dir "data/eventlog-1"}))
 ;(def ^crux.api.ICruxAPI node
 ;  (crux/start-standalone-node {:kv-backend "crux.kv.rocksdb.RocksKv"
 ;                               :db-dir "data/db-dir-1"
 ;                               :event-log-dir "data/eventlog-1"}))
+
+(def storage-dir "data")
+(def embedded-kafka-options
+  {:crux.kafka.embedded/zookeeper-data-dir (str storage-dir "/zookeeper")
+   :crux.kafka.embedded/kafka-log-dir (str storage-dir "/kafka-log")
+   :crux.kafka.embedded/kafka-port 9093})
+
+(defstate embedded-kafka :start (ek/start-embedded-kafka embedded-kafka-options))
+
+(defn new-database []
+  (crux/start-cluster-node
+   {:kv-backend "crux.kv.memdb.MemKv"
+    :bootstrap-servers (get-in embedded-kafka [:options :bootstrap-servers])}))
+
+(defstate ^crux.api.ICruxAPI node :start (new-database))
 
 (defn get-entity [id]
   (crux/entity (crux/db node) id))
