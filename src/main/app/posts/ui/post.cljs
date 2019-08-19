85bc91aa12f431d8950610895d2fc9b158dd8035
Piotr Roterski <pt.roterski@gmail.com>
2019-08-19 21:58:27 +0200

    Add PostShow page

--- src/main/app/posts/ui/post.cljs
+++ src/main/app/posts/ui/post.cljs
@@ -3,12 +3,15 @@
     [app.ui.components :refer [field]]
     [app.posts.ui.profile :refer [Profile ui-profile]]
     [com.fulcrologic.fulcro.dom :as dom :refer [div ul li p h1 h3 button]]
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
     [com.fulcrologic.fulcro.components :as prim :refer [defsc]]
     [com.fulcrologic.fulcro.components :as comp]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [taoensso.timbre :as log]
     [com.fulcrologic.fulcro-css.css :as css]
     [com.fulcrologic.fulcro.algorithms.form-state :as fs]))
+
+(defn id-link [id] (str (namespace id) "/" (name id)))
 
 (defsc Post [this {:post/keys [id title body author]}]
   {:query [:post/id
@@ -18,7 +21,8 @@
    :ident (fn [] [:post/id id])}
   (dom/div :.ui.container.segment
     (div
-      (dom/h5 title)
+      (dom/h5
+        (dom/a {:onClick (fn [] (dr/change-route this ["post-show" (id-link id)]))} title))
       (p body)
       (ui-profile author))))
 
---
+++ src/main/app/posts/ui/post_show.cljs
@@ -0,0 +1,28 @@
+(ns app.posts.ui.post-show
+  (:require
+   [app.posts.ui.post :refer [Post ui-post]]
+   [app.posts.ui.profile :refer [Profile]]
+   [com.fulcrologic.fulcro.dom :as dom :refer [div h1 h2]]
+   [com.fulcrologic.fulcro.components :as prim :refer [defsc]]
+   [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
+   [com.fulcrologic.fulcro.data-fetch :as df]
+   [com.fulcrologic.fulcro.components :as comp]
+   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+   [taoensso.timbre :as log]
+   [com.fulcrologic.fulcro-css.css :as css]))
+
+(defsc PostShow [this {:post/keys [id title body author] :as post}]
+  {:query [:post/id :post/title :post/body {:post/author (comp/get-query Profile)}]
+   :ident :post/id
+   :route-segment ["post-show" :post/id]
+   ;:will-enter (fn [_ {:post/keys [id]}] (dr/route-immediate [:post/id (keyword id)]))}
+   :will-enter (fn [app {:post/keys [id]}]
+                 (let [id (keyword id)]
+                   (dr/route-deferred [:post/id id]
+                      #(df/load app [:post/id id] PostShow
+                                {:post-mutation `dr/target-ready
+                                 :post-mutation-params {:target [:post/id id]}}))))}
+  (div :.ui.container.segment
+    (h1 "Post")
+    (ui-post post)
+    (h2 "Comments")))
