9424fb0336bcc5a1af43a51d6b73c89ac1ade385
Piotr Roterski <pt.roterski@gmail.com>
2019-09-19 19:49:19 +0200

    Change js script path to be absolute not relative

--- src/main/app/server_components/middleware.clj
+++ src/main/app/server_components/middleware.clj
@@ -47,7 +47,7 @@
       [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
      [:body
       [:div#app]
-      [:script {:src "js/main/main.js"}]]]))
+      [:script {:src "/js/main/main.js"}]]]))
 
 ;; ================================================================================
 ;; Workspaces can be accessed via shadow's http server on http://localhost:8023/workspaces.html
